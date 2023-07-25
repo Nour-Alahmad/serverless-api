0b3a9bfb7cd682b7e677b5df6150986888f98776
Nour-Alahmad <Nour.Alahmad1@outlook.com>
2023-07-25 19:49:44 +0000

    done

---
+++ handleCreate/index.js
@@ -0,0 +1,24 @@
+'use strict';
+const peopleModel = require('./people.schema');
+
+exports.handler = async (event) => {
+    let { id, name, email } = event.queryStringParameters;
+    let newPeople = { id, name, email };
+    let res = {
+        statusCode: null,
+        body: null
+    }
+    try {
+        let newPerson = await peopleModel.create(newPeople);
+        res.statusCode = 200;
+        res.body = JSON.stringify(newPerson)
+    }
+    catch (error) {
+        res.statusCode = 500;
+        res.body = JSON.stringify(error.message);
+    }
+
+    return res;
+    
+
+}
